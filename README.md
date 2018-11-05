081bd797c8d0f9e812501699f5f1457648e439f8
Adorax <jeremy.maret@gmail.com>
2018-11-05 18:18:57 +0200

    Project init

---
+++ README.md
@@ -0,0 +1,4 @@
+SYSK Backend
+
+This is the REST API for the mobile application SYSK (Spots You Should Know)
+
---
+++ src/main/java/tech/maret/syskapi/SyskApiApplication.java
@@ -0,0 +1,12 @@
+package tech.maret.syskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyskApiApplication.class, args);
+	}
+}

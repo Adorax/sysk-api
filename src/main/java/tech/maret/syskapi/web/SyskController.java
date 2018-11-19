d2dc835a0b5623732a3cb4665237ce2a6362c287
Adorax <jeremy.maret@gmail.com>
2018-11-19 18:11:45 +0200

    insert login fun and remove useless import

--- src/main/java/tech/maret/syskapi/web/SyskController.java
+++ src/main/java/tech/maret/syskapi/web/SyskController.java
@@ -1,20 +1,10 @@
 package tech.maret.syskapi.web;
 
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import tech.maret.syskapi.domain.User;
-import tech.maret.syskapi.domain.UserAPI;
-import tech.maret.syskapi.domain.UserRepository;
 
 
 @Controller
@@ -23,6 +13,14 @@ public class SyskController {
 	
 	
 	// --- Admin section -----------------------------------------------------------------------
+	
+	/**
+	 * Login redirection
+	 * @param 
+	 * @return login page
+	 */
+	@GetMapping("/login")
+	public String login() { return "login";	}
 	
 	/**
 	 * Show the admin page
